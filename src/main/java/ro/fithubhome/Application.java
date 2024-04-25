1df5c6a133d1d4f4920225cf3d8f76bdf1c14f7d
SimidaR <simida15@yahoo.com>
2024-04-25 16:01:05 +0300

    create folder structure

---
+++ src/main/java/ro/fithubhome/Application.java
@@ -0,0 +1,7 @@
+package ro.fithubhome;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Welcome to FithubHome! Here you can see body stats ");
+    }
+}
